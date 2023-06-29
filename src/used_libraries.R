ee32a87d8220cd6f20cce19053959514ac3b7127
messamat <messamat@uw.edu>
2023-06-29 05:18:28 -0400

    configure renv. will need to be fine-tuned

--- src/used_libraries.R
+++ src/used_libraries.R
@@ -1,17 +1,19 @@
+#suppressPackageStartupMessages(library(CALIBERdatamanage))
+suppressPackageStartupMessages(library(caret))
 suppressPackageStartupMessages(library(mlr3verse))
 suppressPackageStartupMessages(library(plyr))
-suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(rsample))
-suppressPackageStartupMessages(library(caret))
+suppressPackageStartupMessages(library(tidyverse))
+
+
 suppressPackageStartupMessages(library(partykit))
-suppressPackageStartupMessages(library(mlr3extralearners))
+#suppressPackageStartupMessages(library(mlr3extralearners))
 suppressPackageStartupMessages(library(paradox))
 suppressPackageStartupMessages(library(rprojroot))
 suppressPackageStartupMessages(library(ranger))
-suppressPackageStartupMessages(library(CALIBERdatamanage))
-suppressPackageStartupMessages(library(drake))
+
 suppressPackageStartupMessages(library(ggplot2))
-suppressPackageStartupMessages(library(edarf))
+#suppressPackageStartupMessages(library(edarf)) find alternative
 suppressPackageStartupMessages(library(gridExtra))
 suppressPackageStartupMessages(library(qs))
 suppressPackageStartupMessages(library(data.table))
